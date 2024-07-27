ebe566be01117f9b6e684b013cfbb915d3844218
Joshua Billing <joshua.billing9@gmail.com>
2024-07-27 22:52:07 +1200

    Moved types to their own file

--- src/auth.ts
+++ src/auth.ts
@@ -1,28 +1,37 @@
 import NextAuth, { type DefaultSession } from "next-auth";
-import { JWT } from 'next-auth/jwt'
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "./lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
-import { UserRole } from "@prisma/client";
-
-declare module "next-auth" {
-  interface Session {
-    user: {
-      role: UserRole
-    } & DefaultSession["user"]
-  }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    role
-  }
-}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  pages: {
+    error: "/auth/error",
+    signIn: "/auth/login",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
+    async signIn({ user, account }) {
+      // Allow oauth sign in without a verified email.
+      if (account.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id);
+
+      // Prevent sign in without email verification.
+      if (!existingUser.emailVerified) return false;
+
+      // TODO: Add 2fa check.
+
+      return true;
+    },
     async session({ token, session }) {
       if (session.user) {
         if (token.sub) session.user.id = token.sub;
@@ -36,7 +45,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if (!existingUser) return token;
       token.role = existingUser.role;
       return token;
-    }
+    },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
