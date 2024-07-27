b112404d8ab65d118c851bdb4a2e020958296610
Joshua Billing <joshua.billing9@gmail.com>
2024-07-27 22:54:55 +1200

    Auth provider buttons active

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -1,30 +1,37 @@
-"use client"
+"use client";
 
-import { FcGoogle } from "react-icons/fc"
-import { FaFacebook } from "react-icons/fa"
+import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
-    return (
-        <div className="flex itemx-center w-full gap-x-2">
-            <Button
-                size="lg"
-                className="w-full"
-                variant="outline"
-                onClick={() => {}}
-            >
-                <FcGoogle className="h-5 w-5" />
-            </Button>
-            <Button
-                size="lg"
-                className="w-full"
-                variant="outline"
-                onClick={() => {}}
-            >
-                <FaFacebook className="h-5 w-5" />
-            </Button>
+  const onClick = (provider: "google" | "facebook") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
 
-        </div>
-    )
-}
+  return (
+    <div className="flex itemx-center w-full gap-x-2">
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("google")}
+      >
+        <FcGoogle className="h-5 w-5" />
+      </Button>
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onClick("facebook")}
+      >
+        <FaFacebook className="h-5 w-5" />
+      </Button>
+    </div>
+  );
+};
